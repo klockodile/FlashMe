ab6bb346f2df87b778bc3f9b400f5db76743cd72
April Klocko <aklocko@gmail.com>
2021-05-05 13:23:36 -0700

    second commit

---
+++ src/Layout/Card/Create.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { createCard } from "../../utils/api";
+import { useParams } from "react-router-dom";
+
+import CardForm from "./Form";
+
+function CardCreate() {
+
+    const history = useHistory();
+
+    const { deckId } = useParams();
+
+    function submitHandler(card) {
+        console.log("card:", card)
+        createCard(deckId, card).then((savedCard) => {
+            
+            history.push(`/decks/${deckId}`)
+        }
+            
+        )
+    }
+
+    function cancel() {
+        history.goBack();
+    }
+
+    return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/">
+                            <span className="oi oi-home" /> Home
+                        </Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                        Create Deck
+                    </li>
+                </ol>
+            </nav>
+            <h1>Create Card</h1>
+            <CardForm onCancel={cancel} onSubmit={submitHandler} />
+        </div>
+    )
+}
+
+export default CardCreate;
---
+++ src/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="jumbotron bg-dark">
+      <div className="container text-white">
+        <h1 className="display-4">FlashMe</h1>
+        <p className="lead">See and study with your own eyes.</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
